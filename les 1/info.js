43fc7b8547fb7ad52157e62639cdb216deac17cb
Сергеев Сергей Сергеевич <SSS.18@free.uni-dubna.ru>
2020-09-19 15:52:25 +0300

    add les 1

---
+++ les 1/info.js
@@ -0,0 +1,14 @@
+const obj = {
+    first: 1,
+    second: 2,
+}
+
+const firstA = obj.first
+const secondA = obj.second
+console.log(firstA)
+
+const { first, second } = obj
+console.log(first)
+console.log(second)
+
+console.log(process.env)
